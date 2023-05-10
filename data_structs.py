2d70a06bb279cd4f08c617bdda2b8d477d693ed8
Austin <yuxm.austin2016@outlook.com>
2023-05-09 23:57:51 -0500

    before breacking down util functions

--- data_structs.py
+++ data_structs.py
@@ -1,68 +1,71 @@
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import precision_score
-
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+from typing import Any, Optional, Dict
 
 class NewsDataLoader:
-    def __init__(self, predictors, target) -> None:
+    def __init__(self, predictors: Any, target: Any) -> None:
         self.predictors = predictors
         self.target = target
 
-
 class EvaluationDataLoader:
-    def __init__(self, name_dataset, name_model,
-                 target_actual, target_predicted) -> None:
+    def __init__(self, name_dataset: str, name_model: str,
+                 target_actual: Any, target_predicted: Any) -> None:
         self.name_dataset = name_dataset
         self.name_model = name_model
         self.target_actual = target_actual
         self.target_predicted = target_predicted
 
-
 class ScorePackage:
-    def __init__(self, name_dataset, name_model, scores=None,) -> None:
+    def __init__(self, name_dataset: str, name_model: str, scores: Optional[Dict[str, Any]] = None) -> None:
         self.package = dict()
         self.name_dataset = name_dataset
         self.name_model = name_model
-        if scores == None:
+        
+        if scores is None:
             return
+        
         self.accuracy = round(scores['test_accuracy'].mean(), 3)
         self.precision = round(scores['test_precision'].mean(), 3)
         self.recall = round(scores['test_recall'].mean(), 3)
         self.f1 = round(scores['test_f1'].mean(), 3)
 
-    def update_package(self, accuracy=None, precision=None, recall=None, f1=None):
-        if accuracy:
+    def update_package(self, accuracy: Optional[float] = None, precision: Optional[float] = None,
+                       recall: Optional[float] = None, f1: Optional[float] = None) -> None:
+        if accuracy is not None:
             self.accuracy = round(accuracy, 3)
-        if precision:
+        if precision is not None:
             self.precision = round(precision, 3)
-        if accuracy:
+        if recall is not None:
             self.recall = round(recall, 3)
-        if accuracy:
+        if f1 is not None:
             self.f1 = round(f1, 3)
 
-    def construct_eval(self, evaluation_loader: EvaluationDataLoader):
+    def construct_eval(self, evaluation_loader: EvaluationDataLoader) -> None:
         target_actual = evaluation_loader.target_actual
         target_predicted = evaluation_loader.target_predicted
 
-        test_accuracy = accuracy_score(
-            target_actual, target_predicted)
-        test_recall = recall_score(
-            target_actual, target_predicted)
-        test_precision = precision_score(
-            target_actual, target_predicted)
-        test_f1 = f1_score(target_actual,
-                           target_predicted, average='macro')
-        self.update_package(
-            test_accuracy, test_recall, test_precision, test_f1)
+        test_accuracy = accuracy_score(target_actual, target_predicted)
+        test_recall = recall_score(target_actual, target_predicted)
+        test_precision = precision_score(target_actual, target_predicted)
+        test_f1 = f1_score(target_actual, target_predicted, average='macro')
+        
+        self.update_package(test_accuracy, test_recall, test_precision, test_f1)
 
     def __str__(self) -> str:
         return f'Accuracy: {self.accuracy}, Precision: {self.precision}, Recall: {self.recall}, f1 score: {self.f1}'
 
-    def pretty_print(self):
-        print(
-            f'For <{self.name_model}> evaluated on <{self.name_dataset}> Dataset:')
+    def pretty_print(self) -> None:
+        print(f'For <{self.name_model}> evaluated on <{self.name_dataset}> Dataset:')
         print(f'    - accuracy: {self.accuracy}')
         print(f'    - precisionn: {self.precision}')
         print(f'    - recall: {self.recall}')
         print(f'    - f1 score: {self.f1}')
+        
+class ModelEnum:
+    knn = 'knn'
+    logisticRegression = 'logisticRegression'
+    linearSVM = 'linearSVM'
+    randomForest = 'randomForest'
+    voting = 'voting'
+    stack = 'stack'
+    adaBoost = 'adaBoost'
+    rnn = 'rnn'
--- utils.py
+++ utils.py
@@ -1,11 +1,28 @@
 from sklearn.metrics import ConfusionMatrixDisplay
 from data_structs import NewsDataLoader, ScorePackage, EvaluationDataLoader
 
-
 from sklearn.metrics import ConfusionMatrixDisplay
 
-
 def model_predict(model, name_model, train_news_loader: NewsDataLoader, test_news_loader: NewsDataLoader):
+    """
+    Perform predictions using a machine learning model on the training and test datasets.
+
+    Parameters:
+        model (object): The machine learning model used for prediction.
+        name_model (str): The name of the model.
+        train_news_loader (NewsDataLoader): An instance of the NewsDataLoader class containing the training data.
+        test_news_loader (NewsDataLoader): An instance of the NewsDataLoader class containing the test data.
+
+    Returns:
+        Tuple: A tuple containing the instances of EvaluationDataLoader for the training and test datasets.
+
+    Description:
+        This function takes a machine learning model, along with the training and test data loaders, and performs 
+        predictions using the model. It unpacks the predictors and target variables from the loaders, makes 
+        predictions on the test data, and creates instances of EvaluationDataLoader for the training and test 
+        datasets. These instances contain the predicted and actual target values, which are returned for 
+        further evaluation.
+    """
     # unpack loaders
     train_news_predictors, train_news_target = train_news_loader.predictors, train_news_loader.target
     test_news_predictors, test_news_target = test_news_loader.predictors, test_news_loader.target
@@ -24,14 +41,34 @@ def model_predict(model, name_model, train_news_loader: NewsDataLoader, test_new
         target_actual=test_news_target)
     return (train_evaluation_loader, test_evaluation_loader)
 
-
 def model_evaluate(train_evaluation_loader: EvaluationDataLoader,
-                   test_evaluation_loader: EvaluationDataLoader):
+                   test_evaluation_loader: EvaluationDataLoader,
+                   display_confusion = True):
+    """
+    Evaluate the performance of a model based on the predicted and actual target values.
+
+    Parameters:
+        train_evaluation_loader (EvaluationDataLoader): An instance of EvaluationDataLoader containing 
+            the predicted and actual target values for the training dataset.
+        test_evaluation_loader (EvaluationDataLoader): An instance of EvaluationDataLoader containing 
+            the predicted and actual target values for the test dataset.
+        display_confusion (boolean): switch for displaying the confusion matrix.
+
+    Returns:
+        Tuple: A tuple containing the instances of ScorePackage for the training and test datasets.
+
+    Description:
+        This function evaluates the performance of a model based on the predicted and actual target values. 
+        It takes instances of EvaluationDataLoader for the training and test datasets as input. The function 
+        displays the confusion matrix for the test dataset using ConfusionMatrixDisplay.from_predictions, 
+        constructs instances of ScorePackage based on the evaluation results, prints the evaluation scores, 
+        and returns the instances of ScorePackage containing the evaluation scores.
+    """
     name_model = train_evaluation_loader.name_model
-    # TestConfusionn matrix
-    ConfusionMatrixDisplay.from_predictions(
-        test_evaluation_loader.target_actual,
-        test_evaluation_loader.target_predicted)
+    if display_confusion:
+        ConfusionMatrixDisplay.from_predictions(
+            test_evaluation_loader.target_actual,
+            test_evaluation_loader.target_predicted)
 
     train_score_package = ScorePackage(
         name_dataset='train', name_model=name_model)
@@ -44,13 +81,35 @@ def model_evaluate(train_evaluation_loader: EvaluationDataLoader,
     test_score_package.pretty_print()
     return (train_score_package, test_score_package)
 
+def model_predict_evaluate(model, name_model, 
+                           train_news_loader: NewsDataLoader, 
+                           test_news_loader: NewsDataLoader,
+                           display_confusion = True):
+    """
+    Combine the prediction and evaluation steps for a machine learning model.
 
-def model_predict_evaluate(model, name_model, train_news_loader: NewsDataLoader, test_news_loader: NewsDataLoader):
+    Parameters:
+        model (object): The machine learning model used for prediction.
+        name_model (str): The name of the model.
+        train_news_loader (NewsDataLoader): An instance of NewsDataLoader containing the training data.
+        test_news_loader (NewsDataLoader): An instance of NewsDataLoader containing the test data.
+
+    Returns:
+        Tuple: A tuple containing the instances of ScorePackage for the training and test datasets.
+
+    Description:
+        This function combines the prediction and evaluation steps for a model. It takes a machine 
+        learning model, along with the training and test data loaders, and calls the model_predict 
+        function to obtain the predicted and actual target values. It then calls the model_evaluate 
+        function to evaluate the model's performance based on these values. Finally, it returns the 
+        instances of ScorePackage containing the evaluation scores for the training and test datasets.
+    """
     score_package_train, score_package_test = model_evaluate(
         *model_predict(
             model=model,
             name_model=name_model,
             train_news_loader=train_news_loader,
-            test_news_loader=test_news_loader)
+            test_news_loader=test_news_loader), 
+        display_confusion
     )
     return score_package_train, score_package_test
